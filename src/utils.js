99d84201f189dfe056f240deb4832da115c11c16
magic8baller <lturis@gmail.com>
2019-09-20 21:28:18 +0300

    keykey

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
-export const API_KEY = 'dfb8830642469da106b5d96f608318b8'
-// 'c3e59635f92d7353f9092db3597f78b1';
+export const API_KEY = 'c3e59635f92d7353f9092db3597f78b1';
+// 'dfb8830642469da106b5d96f608318b8'
+
 
 export const URL = `https://www.food2fork.com/api/search?key=${API_KEY}`
 
