42cac5fe82ea6e58bc21a85e9a0beacc1e3b59ad
magic8baller <lturis@gmail.com>
2019-09-20 18:24:40 +0300

    add link to recipe

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import RecipeDetails from './RecipeDetails';
 import {API_KEY} from '../utils'
 const RecipeCard = ({recipe}) => {
-	const {image_url, publisher, title, recipe_id} = recipe;
+	const {image_url, publisher, title, recipe_id, source_url} = recipe;
 	const [showInfo, setShowInfo] = useState(false);
 	const [recipeDetails, setRecipeDetails] = useState([]);
 	const {ingredients, social_rank} = recipeDetails;
@@ -28,7 +28,7 @@ const RecipeCard = ({recipe}) => {
 					<img src={image_url} alt="recipe" className="img-card-top" style={{height: '12rem'}} />
 					<div className="card-body text-capitalize">
 						<h6>{title}</h6>
-						<h6 className="text-warning">From: {publisher}</h6>
+						<h6 className="text-warning"></h6>Get recipe: <a href={source_url}>{publisher}</a>
 					</div>
 					<div className="card-footer">
 						<button key={recipe_id} aria-label='recipe details' style={{margin: '13px'}} className="btn btn-primary text-center" data-id={recipe_id} onClick={handleShowInfo}>Ingredients</button>
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 export const API_KEY = 'c3e59635f92d7353f9092db3597f78b1';
+// dfb8830642469da106b5d96f608318b8
 export const URL = `https://www.food2fork.com/api/search?key=${API_KEY}`
 
 export const DEFAULT_URL = `https://api.myjson.com/bins/t7szj`
