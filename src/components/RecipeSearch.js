3864cb1111e3444a372ba06b04efda51e78fabd2
magic8baller <lturis@gmail.com>
2019-09-20 18:27:36 +0300

    example

--- src/components/RecipeSearch.js
+++ src/components/RecipeSearch.js
@@ -6,10 +6,10 @@ const RecipeSearch = () => {
 
 	return (
 		<div className='container d-flex justify-content-center my-3'>
-			
+
 			<form onSubmit={e => handleSubmit(e)} className="form-inline">
 				<label htmlFor='search'>Search:</label>
-				<input data-age='Love' onChange={e => handleSearchChange(e)} type='text' className="form-control" id='search' placeholder='fried eggs' name='search' />
+				<input data-age='Love' onChange={e => handleSearchChange(e)} type='text' className="form-control" id='search' placeholder='example: fried eggs' name='search' />
 				<button type='submit' className="btn btn-primary"><i className="fa fa-search"></i></button>
 			</form>
 		</div>
